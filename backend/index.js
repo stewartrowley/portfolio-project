94e63066127609a7fb9c8f2732d250dcec032d53
stewartrowley <stewy.rowley@gmail.com>
2022-09-24 23:16:50 -0600

    Added the start of the backend of the website

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./database/connect');
+
+const port = process.env.PORT || 8080;
+const app = express();
+
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to Database and listening on ${port}`);
+  }
+});
